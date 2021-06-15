4a3ed6497f2b33fb474296b7be97d6da3d31e0e6
Heja <hejaratsi@gmail.com>
2021-06-15 16:57:38 +0200

    review env secret but all done in dynamic programming

--- drl_lib/to_do/dynamic_programming.py
+++ drl_lib/to_do/dynamic_programming.py
@@ -356,6 +356,8 @@ def policy_evaluation_on_secret_env1() -> ValueFunction:
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
     Returns the Value function (V(s)) of this policy
     """
+
+    # TODO
     env = Env1()
     States_Secret1 = env.states()
     Actions_Secret1 = env.actions()
@@ -395,7 +397,6 @@ def policy_evaluation_on_secret_env1() -> ValueFunction:
 
     return V
 
-    # TODO
     pass
 
 
@@ -407,7 +408,64 @@ def policy_iteration_on_secret_env1() -> PolicyAndValueFunction:
     Returns the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
+    States_Secret1 = env.states()
+    Actions_Secret1 = env.actions()
+    Rewards_Secret1 = env.rewards()
+
     # TODO
+    # La stratégie/policy
+    pi = np.zeros((len(States_Secret1), len(Actions_Secret1)))
+    for s in States_Secret1:
+        pi[s, random.randint(0, len(Actions_Secret1) - 1)] = 1.0
+
+    # La value function
+    V = np.zeros((len(States_Secret1),))
+
+    while True:
+        # 2 : Policy Evaluation
+
+        theta = 0.000001
+        gamma = 0.99999
+
+        while True:
+            delta = 0
+            for s in States_Secret1:
+                v = V[s]
+                V[s] = 0
+                for a in Actions_Secret1:
+                    for s_p in States_Secret1:
+                        for r_idx, r in enumerate(Rewards_Secret1):
+                            V[s] += pi[s, a] * env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+                delta = max(delta, abs(v - V[s]))
+
+            if delta < theta:
+                break
+
+        # 3 : Policy Improvement
+        policy_stable = True
+        for s in States_Secret1:
+            old_state_policy = np.copy(pi[s, :])
+
+            best_a = -1
+            best_a_score = None
+
+            for a in Actions_Secret1:
+                a_score = 0.0
+                for s_p in States_Secret1:
+                    for r_idx, r in enumerate(Rewards_Secret1):
+                        a_score += env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+                if best_a_score is None or best_a_score < a_score:
+                    best_a = a
+                    best_a_score = a_score
+            pi[s, :] = 0.0
+            pi[s, best_a] = 1.0
+            if not np.array_equal(old_state_policy, pi[s]):
+                policy_stable = False
+        if policy_stable:
+            break
+
+    return pi, V
+
     pass
 
 
@@ -418,7 +476,60 @@ def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
     Prints the Policy (Pi(s,a)) and its Value Function (V(s))
     """
     env = Env1()
+    States_Secret1 = env.states()
+    Actions_Secret1 = env.actions()
+    Rewards_Secret1 = env.rewards()
+
     # TODO
+    V = np.zeros((len(States_Secret1),))
+
+    theta = 0.0001
+    gamma = 0.9999
+    while True:
+        delta = 0
+        for s in States_Secret1:
+            v = V[s]
+            V[s] = 0
+            max_cumul_A = 0
+            for a in Actions_Secret1:
+                cumul_A = 0
+                for s_p in States_Secret1:
+                    for r_idx, r in enumerate(Rewards_Secret1):
+                        cumul_A += env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+
+                if cumul_A > max_cumul_A:
+                    max_cumul_A = cumul_A
+
+            V[s] = max_cumul_A
+
+            delta = max(delta, abs(v - V[s]))
+
+        if delta < theta:
+            break
+
+    # ----------------------------------------OUTPUT A DETERMINITSI POLICY----------------------------------------
+    # ----------------------------------------OUTPUT A DETERMINITSI POLICY----------------------------------------
+
+    pi = np.zeros((len(States_Secret1), len(Actions_Secret1)))
+
+
+    for s in States_Secret1:
+        best_a = -1
+        best_a_score = None
+
+        for a in Actions_Secret1:
+            a_score = 0.0
+            for s_p in States_Secret1:
+                for r_idx, r in enumerate(Rewards_Secret1):
+                    a_score += env.transition_probability(s, a, s_p, r_idx) * (r + gamma * V[s_p])
+            if best_a_score is None or best_a_score < a_score:
+                best_a = a
+                best_a_score = a_score
+        pi[s, :] = 0.0
+        pi[s, best_a] = 1.0
+
+    return pi, V
+
     pass
 
 
